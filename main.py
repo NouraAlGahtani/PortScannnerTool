06ddb0c0e43fbb08d5de74b3f20785d186e2e632
NOURA_ALGAHTANI <NouraHani_hotmail.com>
2023-05-29 11:31:19 +0300

    finished

--- main.py
+++ main.py
@@ -10,4 +10,10 @@ print("Scanning target", target, "From port", min_port, "to port", max_port)
 
 
 for port in range (min_port, max_port + 1) :
-    s = socket.socket(socket.AF_INET)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.settimeout(0, 5)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print("port", port ,"is open")
+    s.close
+    
