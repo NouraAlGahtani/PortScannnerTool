dbd1ce358154f5a934a37e2491d69737bcaed96e
NOURA_ALGAHTANI <NouraHani_hotmail.com>
2023-05-29 11:26:57 +0300

    Loop left

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import socket
 
+
+
+target =  input("Enter the Target IP Adresss")
+min_port = int(input("enter the minimum port number you want to scan:"))
+max_port = int(input("enter the maximum port number you want to scan"))
+
+print("Scanning target", target, "From port", min_port, "to port", max_port)
+
+
+for port in range (min_port, max_port + 1) :
+    s = socket.socket(socket.AF_INET)
